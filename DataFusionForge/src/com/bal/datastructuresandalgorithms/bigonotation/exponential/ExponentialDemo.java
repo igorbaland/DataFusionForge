553aa4aede4709919bcbdedf065af2fbc2dcf81a
Igor <igor.balandin@gmx.de>
2023-11-26 14:37:07 +0100

    O(2^n)

---
+++ DataFusionForge/src/com/bal/datastructuresandalgorithms/bigonotation/exponential/ExponentialDemo.java
@@ -0,0 +1,12 @@
+package com.bal.datastructuresandalgorithms.bigonotation.exponential;
+
+public class ExponentialDemo {
+    /*
+    O(2^n)
+    Exponential is that an opposite of logarithmic.
+        Logarithmic slow down as the input  size grown, but exponential growth faster and faster. All the algorithm
+        runs in the exponential type is not scalable at all. They become very slow. We will see the example of
+        exponential in the future
+
+     */
+}
