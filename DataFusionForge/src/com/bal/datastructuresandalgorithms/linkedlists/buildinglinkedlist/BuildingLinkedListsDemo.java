f0a8fc2987de4360622882edfb47b519355c8b31
Igor <igor.balandin@gmx.de>
2023-11-30 08:44:14 +0100

    Building linked lists.

---
+++ DataFusionForge/src/com/bal/datastructuresandalgorithms/linkedlists/buildinglinkedlist/BuildingLinkedListsDemo.java
@@ -0,0 +1,23 @@
+package com.bal.datastructuresandalgorithms.linkedlists.buildinglinkedlist;
+
+import com.bal.datastructuresandalgorithms.linkedlists.workingwithlinkedlists.LinkedListsDemo;
+
+import java.util.Arrays;
+
+public class BuildingLinkedListsDemo {
+    public static void show() {
+        var list = new LinkedList();
+        list.addFirst(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+//        System.out.println(list.getKthFromTheEnd(6));
+//        list.reverse();
+//        var array = list.toArray();
+//        System.out.println(Arrays.toString(array));
+//        list.removeLast();
+//        System.out.println(list.size());
+
+    }
+}
---
+++ DataFusionForge/src/com/bal/datastructuresandalgorithms/linkedlists/buildinglinkedlist/LinkedList.java
@@ -0,0 +1,167 @@
+package com.bal.datastructuresandalgorithms.linkedlists.buildinglinkedlist;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while(current != null) {
+            array[index++] = current.value;
+            current = current.next;
+        }
+
+        return array;
+    }
+
+    public void printMiddle() {
+        var a = first;
+        var b = first;
+        while ( b != last && b.next != last) {
+            b = b.next.next;
+            a = a.next;
+        }
+
+        if (b == last)
+            System.out.println(a.value);
+        else
+            System.out.println(a.value + ", " + a.next.value);
+    }
+
+    public int getKthFromTheEnd(int k) {
+        if(isEmpty())
+            throw new IllegalStateException();
+        var a = first;
+        var b = first;
+        for (var i = 0; i < k -1; i++){
+            b = b.next;
+            if (b == null)
+                new IllegalArgumentException();
+        }
+        while(b != last) {
+            a = a.next;
+            b = b.next;
+        }
+        return a.value;
+    }
+
+    public void reverse() {
+        if(isEmpty()) return;
+        var previous = first;
+        var current = first.next;
+
+        while(current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+
+        size--;
+    }
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+
+        size++;
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = null;
+        else {
+            node.next = last;
+            first = node;
+        }
+
+        size++;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    public void removeFirst() {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(first == last)
+            first = last = null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+
+        size--;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if(current == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+}
