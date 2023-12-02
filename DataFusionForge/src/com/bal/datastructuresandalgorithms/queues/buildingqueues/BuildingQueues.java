f8bf9f3236c2544e745f1eb1c38de1f3ae42a278
Igor <igor.balandin@gmx.de>
2023-12-02 10:29:58 +0100

    building queues from scratch

---
+++ DataFusionForge/src/com/bal/datastructuresandalgorithms/queues/buildingqueues/BuildingQueues.java
@@ -0,0 +1,16 @@
+package com.bal.datastructuresandalgorithms.queues.buildingqueues;
+
+public class BuildingQueues {
+    /*
+    Three ways to implement the queues. We can store in
+        1. Array
+            Circular Arrays
+                Let's imagine our queue looks like this
+                [0, 0, 30, 40, 50]
+                                   R
+                  <---------------
+
+        2. LinkedList
+        3. Stack
+     */
+}
---
+++ DataFusionForge/src/com/bal/datastructuresandalgorithms/queues/buildingqueues/QueueWithTwoStacks.java
@@ -0,0 +1,44 @@
+package com.bal.datastructuresandalgorithms.queues.buildingqueues;
+
+import java.util.Stack;
+
+public class QueueWithTwoStacks {
+    private Stack<Integer> stack1 = new Stack<>();   //enqueue
+    private Stack<Integer> stack2 = new Stack<>();  //dequeue
+
+    //O(1)
+    public void enqueue(int item) {
+        stack1.push(item);
+    }
+
+    //O(n)
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        moveStack1ToStack2();
+
+        return stack2.pop();
+    }
+
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        moveStack1ToStack2();
+
+        return stack2.peek();
+    }
+
+    private void moveStack1ToStack2() {
+        if (stack2.isEmpty())
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+}
