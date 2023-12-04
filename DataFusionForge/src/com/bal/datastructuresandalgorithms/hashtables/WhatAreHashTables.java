4ba8484032a39fc8effd7de3d0df680586b89e1f
Igor <igor.balandin@gmx.de>
2023-12-04 13:50:31 +0100

    What are hash tables?

---
+++ DataFusionForge/src/com/bal/datastructuresandalgorithms/hashtables/WhatAreHashTables.java
@@ -0,0 +1,31 @@
+package com.bal.datastructuresandalgorithms.hashtables;
+
+public class WhatAreHashTables {
+    /*
+    HashTable give us super fast lookups
+        - Spell checkers
+            We can quickly look up a word amongst ten of thousands of words in less then a second.
+        - Dictionaries
+        - Compilers
+            For example, compilers use HashTables to quickly look up the address of function and variables.
+        - Code editors
+        - literally anywhere, we want to lookup super fast.
+
+        Java
+            HashMap
+        JavaScript
+            Object
+        C# and Python
+            Dictionary
+
+    Why are they work super fast?
+        KEY                 VALUE
+        employeeNumber      employee
+                                               Address in memory
+                                            ->      []
+        employeeNumber  ->  HASH FUNCTION   ->      []
+
+        Deterministic
+            every time it the same input it will return the same value.
+     */
+}
