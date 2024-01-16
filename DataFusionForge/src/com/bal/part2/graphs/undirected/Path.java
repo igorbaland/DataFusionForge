bd8a7bad065ab7f9458b700d72aff97f3de65a04
Igor <igor.balandin@gmx.de>
2024-01-16 11:05:40 +0100

    Undirected graphs.

---
+++ DataFusionForge/src/com/bal/part2/graphs/undirected/Path.java
@@ -0,0 +1,18 @@
+package com.bal.part2.graphs.undirected;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path {
+    private List<String> nodes = new ArrayList<>();
+
+    public void add(String node) {
+
+        nodes.add(node);
+    }
+
+    @Override
+    public String toString() {
+        return nodes.toString();
+    }
+}
